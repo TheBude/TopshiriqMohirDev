0d0603535462bb1fb04e857a331fdd63f09b4bef
TheBude <husaenbobmuratov192@gmail.com>
2024-03-28 09:34:48 +0500

    Valyuta-Konvertor

--- tps/Program.cs
+++ tps/Program.cs
@@ -5,5 +5,5 @@ string euro = Console.ReadLine();
 int copyEuro = Convert.ToInt32(euro);
 int hisob = copyEuro * 13650;
 
-Console.WriteLine($"Sizning EURo valyutangiz so'mga hisoblaganda: {hisob} So'm");
+Console.WriteLine($"Sizning EURO valyutangiz so'mga hisoblaganda: {hisob} So'm");
 Console.WriteLine("Bizdan foydalanganingiz uchun rahmat!!!");
