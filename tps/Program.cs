cd4974e645f98b090e3ae634e5423983a84b7349
TheBude <husaenbobmuratov192@gmail.com>
2024-03-28 09:01:07 +0500

    yaxshilangan

--- tps/Program.cs
+++ tps/Program.cs
@@ -5,5 +5,4 @@ string radius = Console.ReadLine();
 int copyRadius = Convert.ToInt32(radius);
 Console.WriteLine($"Doira yuzasi S =  {copyRadius * copyRadius * 3.141516}");
 Console.WriteLine($"Doira uzunligi! L =  {2 * copyRadius * 3.141516}");
-Console.WriteLine("");
 Console.WriteLine("Dastur yakunlandi!");
