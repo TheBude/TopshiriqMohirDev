bd1bfe89f5cbc4400ab0c529fa00ff6d90cd671d
TheBude <husaenbobmuratov192@gmail.com>
2024-03-28 08:49:18 +0500

    Add project files.

---
+++ tps/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Assalomu allaykum men doirani yuzi va aylana uzinligini hisoblaydigan dasturman");
+Console.Write("Doira radiusini kiring:  ");
+
+string radius = Console.ReadLine();
+int copyRadius = Convert.ToInt32(radius);
+Console.WriteLine($"Doira yuzasi S =  {copyRadius * copyRadius * 3.141516}");
+Console.WriteLine($"Doira uzunligi L =  {2 * copyRadius * 3.141516}");
+
