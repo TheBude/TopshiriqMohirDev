10b3ad621dbc4d34add828c3729cd1834492f1fa
TheBude <husaenbobmuratov192@gmail.com>
2024-03-29 16:21:42 +0500

    Yoshni-Hisoblash Dasturi

--- tps/Program.cs
+++ tps/Program.cs
@@ -1,12 +1,17 @@
-﻿Console.WriteLine("Salom! Men valyutani so'mga aylantiruvchi dasturman!");
+﻿Console.WriteLine("Assalomu allaykum! ");
+Console.Write("ismingiz: ");
 
-Console.Write("Sizda necha dollar bor iltimos hisoblash uchun kiriting:  ");
-int pul = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Hisoblamoqdamiz...");
-Console.WriteLine("Hisoblab chiqildi!");
-
-Console.WriteLine($"sizning {pul} dolloringizni so'mga aylantirganimizda shuncha {pul * 12250} so'mni tashkil etmoqda.\nBizning xizmatdan foydalanganingizdan xursandmiz!!!");
+string name =  Console.ReadLine();
+Console.WriteLine($"Tanishganimdan Xursandman {name} !");
+Console.Write("Nechanchi yilsiz: ");
 
 
+int year = Convert.ToInt32( Console.ReadLine() );
+int math = 2024 - year;
+Console.WriteLine($"Aha {name} demak siz {math} yoshdasiz.");
 
+Console.WriteLine("Sizning necha kun yashaganingizdi hisoblamaoqdamiz...");
+
+int math1 = math * 365;
+int math2 = math1 * 24;
+Console.WriteLine($"Demak {name} siz {year} dan 2024 yilgacha yashagam kuningiz va yashagan soatingiz \nKun: {math1}\nSoat: {math2}  \nBu juda yaxshi ammo baxt qancha vaqt yashaganlikdamas, qanday yashaganlikda Hurmatli {name}");
